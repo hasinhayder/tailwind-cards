de94a58965ac5dc0b7e4d16118c9dc546005bcfc
Hasin Hayder <countdraculla@gmail.com>
2021-03-13 15:43:07 +0600

    Tailwind Card Components

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,19 +14,21 @@ module.exports = {
         emerald:colors.emerald,
         teal:colors.teal,
         orange:colors.orange,
+        yellow:colors.yellow,
       },
       maxHeight:{
         116:"29rem", /*464px*/
-        125:"31.25rem", /*464px*/
-        140:"35rem", /*464px*/
+        125:"31.25rem", /*500px*/
+        140:"35rem", /*560px*/
       },
       height:{
         116:"29rem", /*464px*/
-        125:"31.25rem", /*464px*/
-        140:"35rem", /*464px*/
+        125:"31.25rem", /*500px*/
+        140:"35rem", /*560px*/
       },
       minHeight:{
-        9:"4.5rem", /*464px*/
+        9:"4.5rem", /*72*/
+        31:"15.5rem", /*248px*/
       },
     },
   },
